ff155ce800ef8791891f029e9147b1f77183960c
geonwoo kwon <46738866+kay01-kwon@users.noreply.github.com>
2021-08-31 16:17:23 +0900

    Add files via upload

--- mobile_control/include/mobile_control/wheel_odometry.hpp
+++ mobile_control/include/mobile_control/wheel_odometry.hpp
@@ -26,6 +26,8 @@ using Eigen::AngleAxisd;
 using nav_msgs::Odometry;
 using geometry_msgs::Twist;
 
+ros::Time TimeStamp;
+
 class WheelOdometry{
 
     public:
@@ -33,14 +35,11 @@ class WheelOdometry{
     void initiate_variables();
     void publisher_set();
     void subscriber_set();
-    void callback_imu(const Imu::ConstPtr& imu_msg);
-    void callback_enc(const vel::ConstPtr& enc_msg);
+    void callback_camera_imu(const Odometry& camera_imu_msg);
+    void callback_enc(const vel& measure_msg);
     void calculate_velocity_position();
-    double calculate_linear_velocity(Vector3d velocity);
     void velocity_pose_publish();
-    Vector3d getTransformVec(double angle1, double angle2, double dt);
     MatrixXd getRotMat(double angle);
-    void angleNormalizer(double &angle);
     
     private:
     // Variables
@@ -56,15 +55,8 @@ class WheelOdometry{
     Quaterniond q_est = Quaterniond::Identity();
     Quaterniond q_init = Quaterniond::Identity();
 
-    bool is_yaw_init = false;
-
     double yaw = 0;
-    double yaw_init = 0;
-    double linear_velocity = 0;
-    double angular_velocity = 0;
-    double velocity_heading = 0;
-    double angle1 = 0;
-    double angle2 = 0;
+    double angular_vel = 0;
 
     // mobile robot spec
     const double wheel_radious = 0.1520/2.0;
@@ -81,7 +73,7 @@ class WheelOdometry{
     ros::NodeHandle nh_;
     ros::Publisher publisher_odom;
     ros::Publisher publisher_twist;
-    ros::Subscriber subscriber_Imu;
+    ros::Subscriber subscriber_camera_imu;
     ros::Subscriber subscriber_encoder;
 
     // ROS Time info
@@ -107,71 +99,41 @@ void WheelOdometry::initiate_variables()
 
 void WheelOdometry::publisher_set()
 {
-    publisher_odom = nh_.advertise<Odometry>("wheel_odom",1);
-    publisher_twist = nh_.advertise<Twist>("vel",1);
+    publisher_odom = nh_.advertise<Odometry>("odom",1);
 }
 
 void WheelOdometry::subscriber_set()
 {
-    subscriber_Imu = nh_.subscribe("/imu", 1, &WheelOdometry::callback_imu,this);
+    subscriber_camera_imu = nh_.subscribe("/camera/odom/sample", 1, &WheelOdometry::callback_camera_imu,this);
     subscriber_encoder = nh_.subscribe("/measure", 1, &WheelOdometry::callback_enc,this);
     
 }
 
-void WheelOdometry::callback_imu(const Imu::ConstPtr& imu_msg)
+void WheelOdometry::callback_camera_imu(const Odometry& camera_imu_msg)
 {
+    double qw,qx,qy,qz;
     
-    q_imu.w() = imu_msg->orientation.w;
-    q_imu.x() = imu_msg->orientation.x;
-    q_imu.y() = imu_msg->orientation.y;
-    q_imu.z() = imu_msg->orientation.z;
+    qw = camera_imu_msg.pose.pose.orientation.w;
+    qx = camera_imu_msg.pose.pose.orientation.x;
+    qy = camera_imu_msg.pose.pose.orientation.y;
+    qz = camera_imu_msg.pose.pose.orientation.z;
     
-   
-    auto euler = q_imu.toRotationMatrix().eulerAngles(0,1,2);
-    
-    if(is_yaw_init == false){
-        yaw_init = euler[2];
-        is_yaw_init = true;
-        ROS_INFO("Initiate yaw");
-    }
-    
-    yaw = euler[2] - yaw_init;
-    angleNormalizer(yaw);
-
-    q_est = AngleAxisd(0,Vector3d::UnitX())
-            * AngleAxisd(0,Vector3d::UnitY())
-            * AngleAxisd(yaw,Vector3d::UnitZ());
-
-    angular_velocity = imu_msg->angular_velocity.z;
-
+    yaw = atan2(2*(qw*qz + qx*qy),1-2*(qy*qy+qz*qz));
+    angular_vel = camera_imu_msg.twist.twist.angular.z;
 }
-
-void WheelOdometry::callback_enc(const vel::ConstPtr& enc_msg)
+void WheelOdometry::callback_enc(const vel& measure_msg)
 {
 
-    measure_val << enc_msg->velocity[0], -(enc_msg->velocity[1]),
-                -(enc_msg->velocity[2]), enc_msg->velocity[3];
+    measure_val << measure_msg.velocity[0], -(measure_msg.velocity[1]),
+                -(measure_msg.velocity[2]), measure_msg.velocity[3];
 
     calculate_velocity_position();
-    std::cout<<"Yaw"<<std::endl;
-    std::cout<<p_est_curr(2)<<std::endl;
 }
 
 void WheelOdometry::calculate_velocity_position()
 {
     v_robot = Jacob * measure_val/gear_ratio*rpm_to_radps;
-    if(fabs(angular_velocity-v_robot(2))<1)     // 1  radps : 52.6944 deg/s
-        v_robot(2) = angular_velocity;
-
-    velocity_heading = atan2(v_robot(1),v_robot(0));
-    linear_velocity = calculate_linear_velocity(v_robot);
-
-    angle1 = velocity_heading + yaw;
-    angle2  = angle1 + v_robot(2)*dt;
-
     v_est = getRotMat(yaw)*v_robot;
-
-
     p_est_curr = p_est_prev + v_est*dt;
     p_est_curr(2) = yaw;
     p_est_prev = p_est_curr;
@@ -185,56 +147,35 @@ void WheelOdometry::velocity_pose_publish()
     dt = t_curr - t_prev;
 
     Odometry wheel_odom;
-    Twist vel;
+
+    TimeStamp = ros::Time::now();
+    wheel_odom.header.stamp = TimeStamp;
+    wheel_odom.child_frame_id = "base_footprint";
+    wheel_odom.header.frame_id = "odom";
     wheel_odom.pose.pose.position.x = p_est_curr(0);
     wheel_odom.pose.pose.position.y = p_est_curr(1);
     
-    wheel_odom.pose.pose.orientation.w = q_est.w();
-    wheel_odom.pose.pose.orientation.x = q_est.x();
-    wheel_odom.pose.pose.orientation.y = q_est.y();
-    wheel_odom.pose.pose.orientation.z = q_est.z();
+    wheel_odom.pose.pose.orientation.w = cos(yaw/2.0);
+    wheel_odom.pose.pose.orientation.x = 0;
+    wheel_odom.pose.pose.orientation.y = 0;
+    wheel_odom.pose.pose.orientation.z = sin(yaw/2.0);
     
     wheel_odom.twist.twist.linear.x = v_robot(0);
     wheel_odom.twist.twist.linear.y = v_robot(1);
-    wheel_odom.twist.twist.angular.z = v_robot(2);
-    
-    vel.linear.x = v_robot(0);
-    vel.linear.y = v_robot(1);
-    vel.angular.z = v_robot(2);
+    wheel_odom.twist.twist.angular.z = angular_vel;    
     
     publisher_odom.publish(wheel_odom);
-    publisher_twist.publish(vel);
+
     t_prev = t_curr;
-}
-
-double WheelOdometry::calculate_linear_velocity(Vector3d velocity)
-{
-    return sqrt(pow(velocity(0),2)+pow(velocity(1),2));
-}
-
-Vector3d WheelOdometry::getTransformVec(double angle1, double angle2, double dt)
-{
-    Vector3d TfVec;
-    TfVec << sin(angle1+angle2)-sin(angle1),
-            -cos(angle1+angle2)+cos(angle1),
-            angle1-angle2;
-
-    return TfVec;
-
 }
 
 MatrixXd WheelOdometry::getRotMat(double angle)
 {
     MatrixXd R = MatrixXd(3,3);
     R << cos(angle), -sin(angle), 0,
-        sin(angle), sin(angle), 0,
+        sin(angle), cos(angle), 0,
         0,  0,  1;
 
     return R;
 
-}
-
-void WheelOdometry::angleNormalizer(double& angle)
-{
-    angle = atan2(sin(angle),cos(angle));
 }
